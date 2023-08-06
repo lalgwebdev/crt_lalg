2298f0e28097ca7cf893a3ba12bce0b122c14dcb
lalgwebdev <47321570+lalgwebdev@users.noreply.github.com>
2023-08-06 13:27:28 +0100

    Fix attachment for article jump

--- js/lalg-select-jump.js
+++ js/lalg-select-jump.js
@@ -1,8 +1,14 @@
-(function ($) {
-  $("#lalg-article-jump").change(function () {
-      var t=$("#edit-jump").val();
-      if(t != "") {
-        window.location.href = t;
-      }
-  });  
-})(jQuery);
+(function ($, Drupal) {
+  Drupal.behaviors.articleJump = {
+    attach: function attach(context) {
+      once('articleJump', 'select[name="articles"]', context).forEach(function(value) {
+        $('select[name="articles"]').change(function () {
+          var t=$(this).val();
+          if(t != "") {
+            window.location.href = t;
+          }
+        });
+      });                                                           
+    }
+  }
+})(jQuery, Drupal);
